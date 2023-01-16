6ae2b263e6a553f513c7d9c8d8d5e72bf1111088
owusu kenneth gyamfi <49051049+okwesi@users.noreply.github.com>
2023-01-16 10:21:33 +0000

    Update Queue-List.py

--- Queue-List.py
+++ Queue-List.py
@@ -15,3 +15,17 @@ class Queue:
     def printQueue(self):
         print(self.queue)
    
+queue = Queue()
+queue.enqueue(34)
+queue.enqueue(45)
+queue.enqueue(34)
+queue.enqueue(428)
+queue.printQueue()
+queue.dequeue()
+queue.dequeue()
+queue.printQueue()
+queue.enqueue([45,5,7,9])
+queue.printQueue()
+queue.enqueue(["pass", "Fail", "yes"])
+queue.printQueue()
+            
