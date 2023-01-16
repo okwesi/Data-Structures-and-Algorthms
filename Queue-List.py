48ad1cdc5fe88051e75cf58a5ea68ba37a027f2d
owusu kenneth gyamfi <49051049+okwesi@users.noreply.github.com>
2023-01-16 10:21:05 +0000

    Update Queue-List.py

--- Queue-List.py
+++ Queue-List.py
@@ -1,1 +1,17 @@
 
+class Queue:
+    def __init__(self):
+       self.queue = []
+        
+    
+    def enqueue(self, data):
+        self.queue.append(data)
+           
+            
+    def dequeue(self):
+       if len(self.queue) >= 1:
+            self.queue.pop(0)
+    
+    def printQueue(self):
+        print(self.queue)
+   
